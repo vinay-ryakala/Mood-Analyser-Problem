0458b3af2c86aee10f8a023ece3d5f8e8c29203e
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-06 15:09:34 +0530

    ADDED:ablity to handle null exception

--- src/main/java/com/happyMood/MoodAnalyser.java
+++ src/main/java/com/happyMood/MoodAnalyser.java
@@ -4,9 +4,12 @@ public class MoodAnalyser
 {
     public String analyseMood(String message)
     {
-       if (message.contains("sad"))
-           return "SAD";
-       else
-           return "HAPPY";
+
+          if (message.contains("sad"))
+            return "SAD";
+           else
+             return "HAPPY";
+
+
     }
 }
--- src/test/java/com/happyMood/MoodAnalyserTest.java
+++ src/test/java/com/happyMood/MoodAnalyserTest.java
@@ -6,17 +6,34 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     //Welcome to Mood Analyzer
     @Test
-    public void whenSadMoodReturnSad() {
+    public void whenSadMoodReturnSad()
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in sad mood");
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
-    public void whenNotSadReturnHappy() {
+    public void whenNotSadReturnHappy()
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Happy mood");
         Assert.assertEquals("HAPPY", mood);
+    }
+    @Test
+    public void whenNullCatchException()
+    {
+        try
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String mood = moodAnalyser.analyseMood(null);
+            Assert.assertEquals("HAPPY", mood);
+        }
+        catch(NullPointerException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 }
